5868376dba44483ce37f85fce1268dd152f288a1
OlgaChumak13 <olgabuh10@ukr.net>
2023-10-20 17:11:17 +0100

    Dadelete personal data from config file

--- group-7-api/config.py
+++ group-7-api/config.py
@@ -1,6 +1,6 @@
-HOST = "Localhost"
-USER = "Olga"
-PASSWORD = "Hj,jnf23"
+HOST = "x"
+USER = "x"
+PASSWORD = "x"
 
 # different team members to use their own config.py files with their own configuration settings. 
 # This enables all to work with the database without affecting the core code.
