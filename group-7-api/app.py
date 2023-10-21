2c5fb88b44f7fd9153e761adb6c1267cc5c5f569
Rosalind <rosalind.hook@gmail.com>
2023-10-21 19:59:24 +0100

    add to get_books function in app.py to support functionality in main.py

--- group-7-api/app.py
+++ group-7-api/app.py
@@ -7,10 +7,22 @@ def get_authors():
     result = get_authors_records()
     return jsonify(result)
 
-@app.route('/books')
+@app.route('/books', methods=['GET'])
 def get_books():
     books = db_utils.get_all_books()
-    return jsonify(books)
+    if books:
+        books_data = [
+            {
+                "BookID": book_id,
+                "Title": title,
+                "Author": author,
+                "Price": price
+            }
+            for book_id, title, author, price in books
+        ]
+        return jsonify(books_data)
+    else:
+        return jsonify([])  # if no books found, return empty list
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
