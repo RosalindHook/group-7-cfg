106703645c089707c8d16ee30543fa295a47a3a8
Ros <123762126+RosalindHook@users.noreply.github.com>
2023-10-21 17:39:03 +0100

    Update config.py
    remove personal details

--- group-7-api/config.py
+++ group-7-api/config.py
@@ -1,6 +1,6 @@
-HOST = "localhost"
-USER = "root"
-PASSWORD = "Beastie7064"
+HOST = "X"
+USER = "X"
+PASSWORD = "X"
 
 # different team members to use their own config.py files with their own configuration settings. 
 # This enables all to work with the database without affecting the core code.
