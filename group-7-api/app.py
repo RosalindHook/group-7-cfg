d033e05d43322c9bd9c66a8c90d47a2fed3372f5
Sriya Vemuru <sriyavemuru@gmail.com>
2023-10-21 17:22:02 +0100

    get query to get books

--- group-7-api/app.py
+++ group-7-api/app.py
@@ -7,10 +7,5 @@ def get_authors():
     result = get_authors_records()
     return jsonify(result)
 
-@app.route('/books')
-def get_books():
-    books = db_utils.get_all_books()
-    return jsonify(books)
-
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
