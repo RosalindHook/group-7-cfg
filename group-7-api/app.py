94fe6249cbdf87140fae68e615a4752b2c75806b
Rosalind <rosalind.hook@gmail.com>
2023-10-22 14:24:44 +0100

    new buy-book route

--- group-7-api/app.py
+++ group-7-api/app.py
@@ -7,6 +7,7 @@ def get_authors():
     result = get_authors_records()
     return jsonify(result)
 
+
 @app.route('/books', methods=['GET'])
 def get_books():
     books = get_all_books()
@@ -23,6 +24,32 @@ def get_books():
         return jsonify(books_data)
     else:
         return jsonify([])  # if no books found, return empty list
+
+
+@app.route('/buy-book', methods=['POST'])
+def buy_book():
+    book_data = request.json  # Get book information from the POST request in main.py
+
+    book_id = book_data.get("book_id")
+    branch_id = book_data.get("branch_id")
+
+    # Check book availability and stock
+    availability_result, stock = db_utils.check_book_availability(book_id, branch_id)
+
+    if availability_result:
+        if stock > 0:
+            # Update the stock in your database (decrease stock count by 1)
+            db_utils.update_book_stock(book_id, branch_id, stock - 1)
+
+            # Record the purchase by inserting a record into the databse
+            db_utils.record_purchase(book_id, branch_id)
+
+            return jsonify({"message": "Book purchase successful"}), 200
+        else:
+            return jsonify({"message": "The book is out of stock in this branch"}), 400
+    else:
+        return jsonify({"message": "The book is not available"}), 400
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
